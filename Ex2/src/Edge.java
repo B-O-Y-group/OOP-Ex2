79a998f875ee476297e95f49aad34bc423b2d5d6
orondovev <123dovev@gmail.com>
2021-12-12 20:34:53 +0200

    from laptop not sure

--- Ex2/src/Edge.java
+++ Ex2/src/Edge.java
@@ -33,7 +33,7 @@ public class Edge implements EdgeData, Comparable<EdgeData> {
 
     @Override
     public String getInfo() {
-        return null;
+        return "src: " + this.src + " dest: " + this.dest + " weight: " + this.weight;
     }
 
     @Override
